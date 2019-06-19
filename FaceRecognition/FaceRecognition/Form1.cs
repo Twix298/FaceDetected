1451d5482dbe0d0ffbc29264d2fc307ede09f631
Twix298 <48965303+Twix298@users.noreply.github.com>
2019-06-19 17:14:21 +0300

    Add files via upload

---
+++ FaceRecognition/FaceRecognition/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace FaceRecognition
+{
+    public partial class Form1 : Form
+    {
+        MCvFont font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_TRIPLEX, 0.6d, 0.6d);
+        HaarCascade faceDetected;
+        Image<Bgr, Byte> Frame;
+        Capture camera;
+        Image<Gray, byte> result;
+        Image<Gray, byte> TrainedFace = null;
+        Image<Gray, byte> grayFace = null;
+        List<Image<Gray, byte>> trainingImages = new List<Image<Gray, byte>>();
+        List<string> labels = new List<string>();
+        List<string> Users = new List<string>();
+        int count, numLabels, t;
+        string name, names = null;
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            count = count + 1;
+            grayFace = camera.QueryGrayFrame().Resize(320.240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            MCvAvgComp[][] detectedFace = grayFace.DetectHaarCascade(faceDetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20)); 
+            foreach (MCvAvgComp f in detectedFace[0])
+            {
+                TrainedFace = Frame.Copy(f.rect).Convert<Gray, byte>();
+                break;
+            }
+            TrainedFace = result.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            trainingImages.Add(TrainedFace);
+            labels.Add(textBox1.Text);
+            File.WriteAllText(Application.StartupPath + "/Faces/Faces.txt", trainingImages.ToArray().Length.ToString() + ",");
+            for(int i = 1; i < trainingImages.ToArray().Length +1; i++)
+            {
+                trainingImages.ToArray()[i - 1].Save(Application.StartupPath + "/Faces/face" + i + ".bmp");
+                File.AppendAllText(Application.StartupPath + "/Faces/Faces.txt", trainingImages.ToArray().Length.ToString() + ",");
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            faceDetected = new HaarCascade("haarcascade_frontalface_default.xml");
+            try
+            {
+                string labelinf = File.ReadAllText(Application.StartupPath + "/Faces/Faces.txt");
+                string[] Labels = labelinf.Split(',');
+                numLabels = Convert.ToInt16(labels[0]);
+                count = numLabels;
+                string facesLoad;
+                for(int i = 0; i < numLabels+1; i++)
+                {
+                    facesLoad = "face" + i + ".bmp";
+                    trainingImages.Add(new Image<Gray, byte>(Application.StartupPath + "/Faces/Faces.txt"));
+                    labels.Add(Labels[i]);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Nothing in database");
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            camera = new Capture();
+            camera.QueryFrame();
+            Application.Idle += new EventHandler(FrameProcedure);
+        }
+
+        private void FrameProcedure(object sender, EventArgs e)
+        {
+            Users.Add("");
+            Frame = camera.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            grayFace = Frame.Convert<Gray, Byte>();
+            MCvAvgComp[][] facesDetectedNow = grayFace.DetectHaarCascade(faceDetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+            foreach(MCvAvgComp f in facesDetectedNow[0])
+            {
+                result = Frame.Copy(f.rect).Convert<Gray, Byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                Frame.Draw(f.rect, new Bgr(Color.Green), 3);
+                if(trainingImages.ToArray().Length !=0)
+                {
+                    MCvTermCriteria termCriterias = new MCvTermCriteria(count, 0.001);
+                    EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainingImages.ToArray(), labels.ToArray(), 1500, ref termCriterias);
+                    name = recognizer.Recognize(result);
+                    Frame.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.Red));
+                }
+                
+            }
+            Users.Add("");
+            names = "";
+            Users.Clear();
+        }
+    }
+}
