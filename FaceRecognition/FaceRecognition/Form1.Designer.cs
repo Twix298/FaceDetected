5d7c06a2068a911a4429e8036c67497437f7c6b3
Twix298 <48965303+Twix298@users.noreply.github.com>
2019-06-26 17:39:29 +0300

    Add files via upload

--- FaceRecognition/FaceRecognition/Form1.Designer.cs
+++ FaceRecognition/FaceRecognition/Form1.Designer.cs
@@ -30,36 +30,77 @@
         {
             this.components = new System.ComponentModel.Container();
             this.imageBox1 = new Emgu.CV.UI.ImageBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // imageBox1
             // 
-            this.imageBox1.Location = new System.Drawing.Point(63, 52);
+            this.imageBox1.Location = new System.Drawing.Point(3, 3);
             this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(369, 320);
+            this.imageBox1.Size = new System.Drawing.Size(353, 406);
             this.imageBox1.TabIndex = 2;
             this.imageBox1.TabStop = false;
             // 
-            // button1
+            // tableLayoutPanel2
             // 
-            this.button1.Location = new System.Drawing.Point(560, 84);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Распознать";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.imageBox1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(719, 412);
+            this.tableLayoutPanel2.TabIndex = 8;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.imageBox2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 0, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(399, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 79F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 79F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 76F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(280, 406);
+            this.tableLayoutPanel1.TabIndex = 7;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(560, 169);
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(143, 351);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(134, 33);
             this.button2.TabIndex = 4;
             this.button2.Text = "Сохранить";
             this.button2.UseVisualStyleBackColor = true;
@@ -67,45 +108,101 @@
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(491, 135);
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(26, 280);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(111, 21);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Name";
+            this.label1.Text = "Введите имя";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(560, 128);
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBox1.Location = new System.Drawing.Point(143, 280);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(134, 20);
             this.textBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(143, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Распознать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(3, 3);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(134, 166);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 175);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(134, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Добавь в БД";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(143, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Загрузить фотографию";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(3, 333);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(134, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Загрузить фото";
+            this.button5.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.imageBox1);
+            this.ClientSize = new System.Drawing.Size(719, 412);
+            this.Controls.Add(this.tableLayoutPanel2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
         private Emgu.CV.UI.ImageBox imageBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- FaceRecognition/FaceRecognition/Form1.cs
+++ FaceRecognition/FaceRecognition/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -19,6 +20,7 @@ namespace FaceRecognition
         MCvFont font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_TRIPLEX, 0.6d, 0.6d);
         HaarCascade faceDetected;
         Image<Bgr, Byte> Frame;
+        Image<Bgr, Byte> inputFrame;
         Capture camera;
         Image<Gray, byte> result;
         Image<Gray, byte> TrainedFace = null;
@@ -28,25 +30,64 @@ namespace FaceRecognition
         List<string> Users = new List<string>();
         int count, numLabels, t;
         string name, names = null;
+        string pathImage = null;
+        int countString = 0;
 
         private void Button2_Click(object sender, EventArgs e)
         {
-            count = count + 1;
-            grayFace = camera.QueryGrayFrame().Resize(320,240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
-            MCvAvgComp[][] detectedFace = grayFace.DetectHaarCascade(faceDetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20)); 
-            foreach (MCvAvgComp f in detectedFace[0])
+            // сохранение изображения в базу данных
+
+            //count = count + 1;
+            //grayFace = camera.QueryGrayFrame().Resize(320,240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            //MCvAvgComp[][] detectedFace = grayFace.DetectHaarCascade(faceDetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20)); 
+            //foreach (MCvAvgComp f in detectedFace[0])
+            //{
+            //    TrainedFace = Frame.Copy(f.rect).Convert<Gray, byte>();
+            //    break;
+            //}
+            //TrainedFace = result.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            //trainingImages.Add(TrainedFace);
+            //labels.Add(textBox1.Text);
+            //File.WriteAllText(Application.StartupPath + "/Faces/Faces.txt", trainingImages.ToArray().Length.ToString() + ",");
+            //for(int i = 1; i < trainingImages.ToArray().Length +1; i++)
+            //{
+            //    trainingImages.ToArray()[i - 1].Save(Application.StartupPath + "/Faces/face" + i + ".bmp");
+            //    File.AppendAllText(Application.StartupPath + "/Faces/Faces.txt", trainingImages.ToArray().Length.ToString() + ",");
+            //}
+            //textBox1.Clear();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            // загрузка изображения для обработки
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                TrainedFace = Frame.Copy(f.rect).Convert<Gray, byte>();
-                break;
+                pathImage = openFileDialog.FileName;
+                inputFrame = new Image<Bgr, byte>(openFileDialog.FileName);
+                imageBox2.Image = inputFrame;
             }
-            TrainedFace = result.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
-            trainingImages.Add(TrainedFace);
-            labels.Add(textBox1.Text);
-            File.WriteAllText(Application.StartupPath + "/Faces/Faces.txt", trainingImages.ToArray().Length.ToString() + ",");
-            for(int i = 1; i < trainingImages.ToArray().Length +1; i++)
+            else
             {
-                trainingImages.ToArray()[i - 1].Save(Application.StartupPath + "/Faces/face" + i + ".bmp");
-                File.AppendAllText(Application.StartupPath + "/Faces/Faces.txt", trainingImages.ToArray().Length.ToString() + ",");
+                MessageBox.Show("Can't open file");
+            }
+            // обработка
+            Users.Add("");
+            grayFace = inputFrame.Convert<Gray, Byte>();
+            MCvAvgComp[][] facesDetectedNow = grayFace.DetectHaarCascade(faceDetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+            foreach (MCvAvgComp f in facesDetectedNow[0])
+            {
+                result = inputFrame.Copy(f.rect).Convert<Gray, Byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                inputFrame.Draw(f.rect, new Bgr(Color.Green), 3);
+                if (trainingImages.ToArray().Length != 0)
+                {
+                    MCvTermCriteria termCriterias = new MCvTermCriteria(count, 0.001);
+                    EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainingImages.ToArray(), labels.ToArray(), 1500, ref termCriterias);
+                    name = recognizer.Recognize(result);
+                    inputFrame.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.Red));
+                }
+                Users.Add("");
+                
             }
         }
 
@@ -58,10 +99,10 @@ namespace FaceRecognition
             {
                 string labelinf = File.ReadAllText(Application.StartupPath + "/Faces/Faces.txt");
                 string[] Labels = labelinf.Split(',');
-                numLabels = Convert.ToInt16(labels[0]);
+                numLabels = Convert.ToInt32(Labels[0]);
                 count = numLabels;
                 string facesLoad;
-                for(int i = 0; i < numLabels+1; i++)
+                for(int i = 1; i < numLabels+1; i++)
                 {
                     facesLoad = "face" + i + ".bmp";
                     trainingImages.Add(new Image<Gray, byte>(Application.StartupPath + "/Faces/Faces.txt"));
@@ -74,15 +115,31 @@ namespace FaceRecognition
             }
         }
 
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            PutImageInDb(pathImage);
+        }
+
         private void Button1_Click(object sender, EventArgs e)
         {
-            camera = new Capture();
-            camera.QueryFrame();
-            Application.Idle += new EventHandler(FrameProcedure);
+            // создание видеопотока
+            try
+            {
+
+                camera = new Capture();
+                camera.QueryFrame();
+                Application.Idle += new EventHandler(FrameProcedure);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
         }
 
         private void FrameProcedure(object sender, EventArgs e)
         {
+            // обработка изображения фильтром Хаара
             Users.Add("");
             Frame = camera.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
             grayFace = Frame.Convert<Gray, Byte>();
@@ -104,5 +161,88 @@ namespace FaceRecognition
             names = "";
             Users.Clear();
         }
+
+        private void PutImageInDb(string imageFile)
+        {
+            byte[] imageData = null;
+            FileInfo fileInfo = new FileInfo(imageFile);
+            long numBytes = fileInfo.Length;
+            FileStream fileStream = new FileStream(imageFile, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            imageData = binaryReader.ReadBytes((int)numBytes);
+
+            // получение расширения
+            string imageExtension = Path.GetExtension(imageFile).Replace(".", "").ToLower();
+
+            using (SqlConnection sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\vs_project\FaceRecognition\FaceRecognition\FaceDatabase.mdf;Integrated Security=SSPI"))
+            {
+                
+                string commandText = "INSERT INTO FACES (Id, Image, Image_format, Name) VALUES(@Id, @Image, @Image_format, @Name)"; // запрос на вставку
+                SqlCommand command = new SqlCommand(commandText, sqlConnection);
+                string stmt = "SELECT COUNT(*) FROM dbo.Faces";
+                
+                using (SqlCommand commandCount = new SqlCommand(stmt, sqlConnection))
+                {
+                    sqlConnection.Open();
+                    countString = (int)commandCount.ExecuteScalar();
+                    sqlConnection.Close();
+                }
+                command.Parameters.AddWithValue("@Id", countString+1);//записывает порядковый номер
+                command.Parameters.AddWithValue("@Image", (object)imageData); // записываем само изображение
+                command.Parameters.AddWithValue("@Image_format", imageExtension); // записываем расширение изображения
+                if(textBox1.Text == "")
+                {
+                    MessageBox.Show("Введите Имя");
+                }
+                else
+                {
+                    command.Parameters.AddWithValue("@Name", textBox1.Text);
+                    sqlConnection.Open();
+                    command.ExecuteNonQuery();
+                    sqlConnection.Close();
+                }
+               
+            }
+
+        }
+        private void GetImageBinaryFromDb()
+        {
+            List<byte[]> iScreen = new List<byte[]>();
+            List<string> iScreenFormat = new List<string>();
+            using (SqlConnection sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\vs_project\FaceRecognition\FaceRecognition\FaceDatabase.mdf;Integrated Security=SSPI"))
+            {
+                sqlConnection.Open();
+                countString = 0;
+                SqlCommand sqlCommand = new SqlCommand();
+                for (int i = 1; i < countString; i++)
+                    sqlCommand.CommandText = @"SELECT [Image], [Image_format] FROM [Faces] WHERE [Id] = " + Convert.ToString(countString);
+
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+                byte[] iTrimByte = null;
+                string iTrimText = null;
+                while(sqlDataReader.Read())
+                {
+                    iTrimByte = (byte[])sqlDataReader["Image"];
+                    iScreenFormat.Add(iTrimText);
+
+                }
+                sqlConnection.Close();
+            }
+
+            byte[] imageData = iScreen[0]; // возвращает массив байт из БД. Так как у нас SQL вернёт одну запись и в ней хранится нужное нам изображение, то из листа берём единственное значение с индексом '0'
+            MemoryStream ms = new MemoryStream(imageData);
+            Image newImage = Image.FromStream(ms);
+
+            // сохраняем изоражение на диск
+            string iImageExtension = iScreenFormat[0]; // получаем расширение текущего изображения хранящееся в БД
+            string iImageName = @"C:\result_new" + "." + iImageExtension; // задаём путь сохранения и имя нового изображения
+            inputFrame = new Image<Bgr, byte>(iImageName);
+            imageBox2.Image = inputFrame;
+            //if (iImageExtension == "png") { newImage.Save(iImageName, System.Drawing.Imaging.ImageFormat.Png); }
+            //else if (iImageExtension == "jpg" || iImageExtension == "jpeg") { newImage.Save(iImageName, System.Drawing.Imaging.ImageFormat.Jpeg); }
+            //else if (iImageExtension == "gif")
+            //{
+            //    newImage.Save(iImageName, System.Drawing.Imaging.ImageFormat.Gif);
+            }
     }
 }
